0178447ee388e75344a6e64ff0d9cfa0cfdb47c1
Yghaddar <58191171+yghaddar@users.noreply.github.com>
2020-03-16 16:24:56 -0400

    added trycatch

--- functions.js
+++ functions.js
@@ -3,8 +3,19 @@ function getYearOfBirth (age) {
 }
 
 function createGreeting (name, age) {
-  return `Sup, my name is yousef and i am 26 years old. 
+  const yearOfBirth = 2020 - age
+  return `Sup, my name is ${name} and i am 26 years old. 
   I was born in ${yearOfBirth}`
+}
+
+function yearOfBirth (age) {
+  if (age < 0) {
+    throw new Error('Age can not be negative')
+    return 'cant be negative'
+  }
+  try {
+      const greeting1 = createGreeting('yousef', '26')
+  }catch (e)
 }
 
 const greeting1 = createGreeting()
