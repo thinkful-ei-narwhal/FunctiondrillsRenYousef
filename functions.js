4573ee790e74612175857715d79d6fdb1bfa95a5
Yghaddar <58191171+yghaddar@users.noreply.github.com>
2020-03-16 16:03:00 -0400

    modified function

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 function createGreeting (name, age) {
-
-  return 'Sup, my name is yousef and i am 26 years old'
+let yearOfBirth = 2020 - age
+  return `Sup, my name is yousef and i am 26 years old. 
+  I was born in ${yearOfBirth}`
 }
 
 let greeting1 = createGreeting()
